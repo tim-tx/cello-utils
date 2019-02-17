bcd24458dfb491c1337f914951bd266aea00b2ea
Tim Jones <tjones01@gmail.com>
2019-02-17 09:41:00 -0500

    New JSON structure.

--- parameters-json-builder/parameters_json_builder.py
+++ parameters-json-builder/parameters_json_builder.py
@@ -18,19 +18,27 @@ app_config_file = os.path.join(args.directory,"cello2","cello2-" + args.applicat
 with open(app_config_file,"r") as config_json:
     config = json.load(config_json)
 
-stages = [x["name"].lower() for x in config["stages"]]
+stages = [x["name"] for x in config["stages"]]
 
-params = {}
+applications = []
+params = []
+application = {"name":config["name"],"stages":params}
+applications = [application,]
 
 for stage in stages:
-    params[stage] = {}
-    path = os.path.join(args.directory,"cello2","cello2-" + stage,"src","non-packaged-resources","algorithms")
+    stage_params = {}
+    stage_params["name"] = stage
+    stage_params["algorithms"] = []
+    params.append(stage_params)
+    
+    path = os.path.join(args.directory,"cello2","cello2-" + stage.lower(),"src","non-packaged-resources","algorithms")
     algorithms = glob(os.path.join(path,"*"))
     for algorithm in algorithms:
         base = os.path.basename(algorithm)
         alg_param_file = os.path.join(algorithm,base + ".json")
         with open(alg_param_file,"r") as param_json:
-            param = json.load(param_json)
-            params[stage][base] = param
+            algorithm_params = json.load(param_json)
+            stage_params["algorithms"].append(algorithm_params)
+        
 
-print(json.dumps(params,indent=2))
+print(json.dumps(applications,indent=2))
